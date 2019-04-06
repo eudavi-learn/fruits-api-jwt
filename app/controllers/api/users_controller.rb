7db711d17ccf0f7cc2f994b30de49a8f713b2c99
davigl <davi.guimaraesleite@gmail.com>
2019-04-06 19:34:32 -0300

    initial commit

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ActionController::API
+  def create
+  	@user = User.new(user_params)
+
+  	if @user.save!
+  		render json: user.as_json(only: [:id, :email]), status: :created
+  	else
+
+  	end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def user_params
+  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
