8a8319bfa500e23bfd16c51bda531a70c013ea46
Davi Guimarães <davi.guimaraesleite@gmail.com>
2019-05-29 13:15:32 -0300

    fix blank spaces

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,13 +3,13 @@ class Api::UsersController < ActionController::API
   before_action :set_user, only: [:show]
 
   def create
-  	@user = User.new(user_params)
+    @user = User.new(user_params)
   	
     if @user.save
-  		render json: { data: @user.as_json(only: [:name, :email]), status: 201 }
+      render json: { data: @user.as_json(only: [:name, :email]), status: 201 }
     else
       render json: { data: nil, status: 422, messages: @user.errors.full_messages }
-  	end
+    end
   end
 
   def show
@@ -27,6 +27,6 @@ class Api::UsersController < ActionController::API
   end
 
   def user_params
-  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
